1e803c30a4cca6b92f7935788d39386835a856f0
Samet <sam.kem.2000@gmail.com>
2022-06-22 19:58:00 +0300

    second

--- app.js
+++ app.js
@@ -2,9 +2,22 @@ const express = require('express');
 
 const app = express();
 
+//template engine
+app.set("view engine","ejs");
+//middleware
+app.use(express.static("public"));
+
 app.get('/',(req,res)=>{
-    res.send("index page");
+    res.status(200).render('index',{
+        pageName:"index"
+    });
 })
+app.get('/about',(req,res)=>{
+    res.status(200).render('about',{
+        pageName:"about"
+    });
+})
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
